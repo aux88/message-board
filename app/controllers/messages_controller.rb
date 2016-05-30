da63b8cb56f0550000f379e9d0a37e1839b47dbe
yoshimisatou <youhes@gmail.com>
2016-05-30 13:01:11 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
     else
       # メッセージが保存できなかった時
       @messages = Message.all
-      flash.now[:alert]="メッセージの保存に失敗しました"
+      flash.now[:alert]="メッセージの保存に失敗しました。"
       render 'index'
     end
   end
